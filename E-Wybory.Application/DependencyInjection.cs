e8bd7ca030993207367c5f812eb775d22e243b7f
Krzysztof Wróblewski <260394@student.pwr.edu.pl>
2024-10-04 16:21:39 +0200

    Add project files.

---
+++ E-Wybory.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Wybory.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            //Add neccesery services for module here
+
+
+            return services;
+        }
+    }
+}
---
+++ E-Wybory.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Wybory.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            //Add neccesery services for module here
+
+
+            return services;
+        }
+    }
+}
