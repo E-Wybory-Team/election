5a02d37e5b2aa4553823cf39e17f77ce83df2ec2
commdeb <zdzichukw1@gmail.com>
2024-11-12 13:59:31 +0100

    Added template DTOs fro 2fa management

---
+++ E-Wybory.Application/DTOs/TwoFactorAuth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Wybory.Application.DTOs
+{
+    public class TwoFactorAuthVerifyRequest
+    {
+        [Required]
+        public string Code { get; set; } = string.Empty;
+    }
+
+    public class TwoFactorAuthResponse
+    {
+        [Required]
+        public string QRCodeUri { get; } = string.Empty;
+        [Required]
+        public string Secret { get; } = string.Empty;
+
+    }
+}
