0a2e81eaf34e1f62400259c0d0a1d353ce6fa906
commdeb <zdzichukw1@gmail.com>
2024-11-22 16:50:09 +0100

    Adjusted tests

--- E-Wybory.Test/Server/TokenRenewalMiddlewareTests.cs
+++ E-Wybory.Test/Server/TokenRenewalMiddlewareTests.cs
@@ -3,6 +3,8 @@ using E_Wybory.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Moq;
+using NuGet.Common;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -11,7 +13,7 @@ namespace E_Wybory.Tests
     public class TokenRenewalMiddlewareTests
     {
         [Fact]
-        public async Task InvokeAsync_ShouldRenewToken_WhenTokenIsValid()
+        public async Task InvokeAsync_ShouldRenewToken_WhenTokenIsValidAndNearExpiration()
         {
             // Arrange
             var mockTokenService = new Mock<IJWTService>();
@@ -19,7 +21,85 @@ namespace E_Wybory.Tests
                 .ReturnsAsync("newToken");
 
             var context = new DefaultHttpContext();
-            context.Request.Headers["Authorization"] = "Bearer oldToken";
+            context.Request.Headers["Authorization"] = "Bearer validToken";
+
+            var middleware = new TokenRenewalMiddleware(next: (innerHttpContext) => Task.CompletedTask, tokenService: mockTokenService.Object);
+
+            // Mock the token handler and token
+            var mockTokenHandler = new Mock<JsonWebTokenHandler>();
+            mockTokenHandler.Setup(handler => handler.CanReadToken("validToken")).Returns(true);
+            mockTokenHandler.Setup(handler => handler.ReadJsonWebToken("validToken"))
+                .Returns(new JsonWebToken("validToken"));
+
+
+            var token = mockTokenHandler.Object.ReadJsonWebToken("validToken");
+            token.GetType().GetProperty("ValidFrom").SetValue(token, DateTime.UtcNow.AddMinutes(-10));
+            token.GetType().GetProperty("ValidTo").SetValue(token, DateTime.UtcNow.AddMinutes(50));
+
+            // Act
+            await middleware.InvokeAsync(context);
+
+            // Assert
+            Assert.Equal("Bearer newToken", context.Response.Headers["Authorization"]);
+        }
+
+        [Fact]
+        public async Task InvokeAsync_ShouldNotRenewToken_WhenTokenIsNotNearExpiration()
+        {
+            // Arrange
+            var mockTokenService = new Mock<IJWTService>();
+
+            var context = new DefaultHttpContext();
+            context.Request.Headers["Authorization"] = "Bearer validToken";
+
+            var middleware = new TokenRenewalMiddleware(next: (innerHttpContext) => Task.CompletedTask, tokenService: mockTokenService.Object);
+
+            // Mock the token handler and token
+            var mockTokenHandler = new Mock<JsonWebTokenHandler>();
+            mockTokenHandler.Setup(handler => handler.CanReadToken("validToken")).Returns(true);
+            mockTokenHandler.Setup(handler => handler.ReadJsonWebToken("validToken"))
+                .Returns(new JsonWebToken("validToken"));
+
+            var token = mockTokenHandler.Object.ReadJsonWebToken("validToken");
+            token.GetType().GetProperty("ValidFrom").SetValue(token, DateTime.UtcNow.AddMinutes(-10));
+            token.GetType().GetProperty("ValidTo").SetValue(token, DateTime.UtcNow.AddMinutes(50));
+
+            // Act
+            await middleware.InvokeAsync(context);
+
+            // Assert
+            Assert.False(context.Response.Headers.ContainsKey("Authorization"));
+        }
+
+        [Fact]
+        public async Task InvokeAsync_ShouldNotRenewToken_WhenTokenIsInvalid()
+        {
+            // Arrange
+            var mockTokenService = new Mock<IJWTService>();
+
+            var context = new DefaultHttpContext();
+            context.Request.Headers["Authorization"] = "Bearer invalidToken";
+
+            var middleware = new TokenRenewalMiddleware(next: (innerHttpContext) => Task.CompletedTask, tokenService: mockTokenService.Object);
+
+            // Mock the token handler
+            var mockTokenHandler = new Mock<JsonWebTokenHandler>();
+            mockTokenHandler.Setup(handler => handler.CanReadToken("invalidToken")).Returns(false);
+
+            // Act
+            await middleware.InvokeAsync(context);
+
+            // Assert
+            Assert.False(context.Response.Headers.ContainsKey("Authorization"));
+        }
+
+        [Fact]
+        public async Task InvokeAsync_ShouldNotRenewToken_WhenNoTokenProvided()
+        {
+            // Arrange
+            var mockTokenService = new Mock<IJWTService>();
+
+            var context = new DefaultHttpContext();
 
             var middleware = new TokenRenewalMiddleware(next: (innerHttpContext) => Task.CompletedTask, tokenService: mockTokenService.Object);
 
@@ -27,7 +107,7 @@ namespace E_Wybory.Tests
             await middleware.InvokeAsync(context);
 
             // Assert
-            Assert.Equal("Bearer newToken", context.Response.Headers["Authorization"]);
+            Assert.False(context.Response.Headers.ContainsKey("Authorization"));
         }
     }
 }
